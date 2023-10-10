30b52a836a4623a3f4f994f85030bf21dcd704e5
Mario <marioeduardo.garciasalazar@gmail.com>
2023-10-09 20:56:25 -0600

    Arreglos para login

--- db.php
+++ db.php
@@ -2,8 +2,5 @@
 $con = new mysqli("127.0.0.1", "root", "", "login_db");
 $sql = "SELECT * FROM `usuarios`";
 $result = $con -> query($sql);
-foreach ($result as $cada_result){
-    print_r($cada_result);
-}
 $con -> close();
 ?>
--- login.php
+++ login.php
@@ -1,5 +1,12 @@
 <?php 
 $userName = $_POST["user"];
 $userPassword = $_POST["contraseña"];
-echo $userName . "<br>" . $userPassword;
+require_once "db.php";
+
+foreach ($result as $cada_result){
+    // print_r($cada_result["Email"] . "<br>");
+    if ($cada_result["Email"] == $userName && $cada_result["Contraseña"] == $userPassword) {
+        echo "Bienvenido has entrado al login";
+    }
+}
 ?>
