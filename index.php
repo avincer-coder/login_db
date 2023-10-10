02ba7f5be1ced0332db1f13e283930bf18827ed9
Mario <marioeduardo.garciasalazar@gmail.com>
2023-10-10 17:23:31 -0600

    Mas ediciones para el log in y cordinacion con el profile

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <?php 
+
+if (isset($_POST["user"])) {
+    $userName = $_POST["user"];
+    $userPassword = $_POST["contraseña"];
+    require_once "db.php";
+    $sql = "SELECT * FROM `usuarios` WHERE `Email` LIKE '$userName'";
+
+    $result = $con -> query($sql);// Trae la tabla de la base de datos
+    // La consulta (query guardada en la variable sql) la mandamos a la conexion
+
+
+    if (mysqli_num_rows($result) == 0) { //mysqli_num_rows Cuenta cuantas columnas tiene la tabla si no exsisten numeros entonces se cumple y imprime lo de abajo
+        // header("location:index.php");
+        echo "NO EXSISTE NINGUN CORREO";
+
+    }else{
+        $row = $result->fetch_assoc(); //->fetch_assoc() convierte la tabla que viene empaquetada en un objeto en la variable $result, a una array 
+        if ($userPassword == $row["Contraseña"]) { // Si la contraseña es igual a alguna que exsiste entonces redirecciona a otra pagina
+            header("location:profile.php");
+
+        }else {
+            echo "TU CONTRASEÑA ESTA INCORRECTA";
+        }
+    }
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -9,7 +36,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="login.php" method="post">
+    <form action="" method="post">
         <label for="usuario">Usuario</label>
         <input id="usuario" type="text" name="user">
         <input type="password" name="contraseña">
--- login.php
+++ login.php
@@ -1,19 +1,5 @@
 <?php 
-$userName = $_POST["user"];
-$userPassword = $_POST["contraseña"];
-require_once "db.php";
-$sql = "SELECT * FROM `usuarios` WHERE `Email` LIKE '$userName'";
-$result = $con -> query($sql);// Trae la tabla de la base de datos
-// La consulta (query guardada en la variable sql) la mandamos a la conexion
 
-if (mysqli_num_rows($result) == 0) { //mysqli_num_rows Cuenta cuantas columnas tiene la tabla si no exsisten numeros entonces se cumple y imprime lo de abajo
-    echo "NO EXSISTE NINGUN CORREO";
-}else{
-    $row = $result->fetch_assoc(); //->fetch_assoc() convierte la tabla que viene empaquetada en un objeto en la variable $result, en una array 
-    if ($userPassword == $row["Contraseña"]) { // Si la contraseña es igual a alguna que exsiste entonces redirecciona a otra pagina
-        header("location:profile.php");
-    }
-}
 
 
 
