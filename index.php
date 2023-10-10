7abd221957ddd790d9da7e1d97062ebebcd24ccd
Mario <marioeduardo.garciasalazar@gmail.com>
2023-10-09 19:47:35 -0600

    Creacion y conexion a base de datos

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
-echo "Hola a todos"
+
+echo "Hola a todos";
 
 
+$con = new mysqli("127.0.0.1", "root", "", "login_db");
+$sql = "SELECT * FROM `usuarios`";
 
+$result = $con -> query($sql);
 
+foreach ($result as $cada_result){
+    print_r($cada_result);
+}
+
+$con -> close();
 
 ?>
